84845e70557ef6dffffaf14a24e24984a2e2c8e0
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-15 15:34:04 -0500

    this will get all the locations from the database and send them to where the response was made, in this case POSTMAN

--- server/routes/locations.router.js
+++ server/routes/locations.router.js
@@ -7,6 +7,15 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
+  const sqlText = `SELECT * FROM "locations";`
+
+  pool
+    .query(sqlText)
+    .then(result => res.send(result.rows))
+    .catch((err) => {
+      console.log('GET request for locations FAILED: ', err);
+      res.sendStatus(500);
+    });
 });
 
 /**
