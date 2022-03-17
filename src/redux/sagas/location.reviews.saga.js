a11a8e994723d3b346fa235c03dedd7e59d52596
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-16 22:43:28 -0500

    saga created to fetch Location reviews and User reviews

---
+++ src/redux/sagas/location.reviews.saga.js
@@ -0,0 +1,24 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchLocationReviews(action) {
+
+    // console.log(event);
+    
+    try {
+        const reviews = yield axios.get(`/location/${action.payload}`)
+        console.log('The location reviews fetched from database', reviews.data);
+        yield put({ type:'SET_LOCATION_R', payload: reviews.data})
+
+    } catch (error) {
+        console.log('the location reviews were not fetched D:', error);
+
+    }
+
+}
+
+function* locationReviewsSaga() {
+    yield takeLatest('FETCH_LOCATION_R', fetchLocationReviews);
+}
+
+export default locationReviewsSaga
---
+++ src/redux/sagas/user.reviews.saga.js
@@ -0,0 +1,24 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchUserReviews(action) {
+
+    // console.log(event);
+    
+    try {
+        const reviews = yield axios.get(`/user/${action.payload}`)
+        console.log('The user reviews fetched from database', reviews.data);
+        yield put({ type:'SET_LOCATION_R', payload: reviews.data})
+
+    } catch (error) {
+        console.log('the user reviews were not fetched D:', error);
+
+    }
+
+}
+
+function* userReviewsSaga() {
+    yield takeLatest('FETCH_USER_R', fetchUserReviews);
+}
+
+export default userReviewsSaga
