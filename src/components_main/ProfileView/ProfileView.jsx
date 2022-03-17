576eec6be603ed13d122cbd4f31f2771228e5800
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-17 13:12:38 -0500

    component boilerplate created

---
+++ src/components_main/ProfileView/ProfileView.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+function ProfileView () {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default ProfileView
+
+
+
+/* {
+	"create exported component": {
+		"prefix": "componen",
+		"body": [
+			"console.log($1);",
+			"$2"
+		],
+		"description": "Log output to console"
+	}
+} */
