9abe3953a7b30539fb709e8a44bee6a21bad6ba9
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-18 11:17:27 -0500

    THE BUTTON CLICKES!!!!!!!

--- src/components_main/LocationsList/LocationsList.jsx
+++ src/components_main/LocationsList/LocationsList.jsx
@@ -10,17 +10,23 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function LocationsList() {
 
+    const handleClick = () => {
+        console.log('clciky wohoo');
+    }
+
     const locations = useSelector(store => store.locations)
     console.log(locations);
     return (
         <>
             <p>I am the locatiosn weeee</p>
             {locations.map(local => (
-                <div>
-                    <h1>{local.name}</h1>
-                    <h3>{local.description}</h3>
-                    <button>MORE INFO</button>
-                </div>
+                <>
+                    <div key={local.id}>
+                        <h1>{local.name}</h1>
+                        <h3>{local.description}</h3>
+                        <button onClick={handleClick}>MORE INFO</button>
+                    </div>
+                </>
             ))}
         </>
     )
