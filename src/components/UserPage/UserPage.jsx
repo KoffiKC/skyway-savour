ac047357767547079afd3e8725143cf0cf0ecf5f
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-19 16:05:55 -0500

    Basic mui attributes added to modal for submitting the form

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import ReviewForm from '../../components_main/ReviewForm/ReviewForm';
+
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -9,6 +11,8 @@ function UserPage() {
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <p>is it here?</p>
+            <ReviewForm/>
       <LogOutButton className="btn" />
     </div>
   );
