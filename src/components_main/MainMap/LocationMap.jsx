34d03fa4221f219208864cbc2b9501f79aad72cf
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-24 14:34:13 -0500

    map component to for each loaction view create

---
+++ src/components_main/MainMap/LocationMap.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import GoogleMapReact from 'google-map-react'
+import './map.css'
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/map-marker'
+// import baselinemylocation from '@iconify/icons-mdi/baseline-my-location'
+import { useDispatch, useSelector } from 'react-redux';
+
+
+
+
+const LocationPin = ({ text }) => (
+    <div className="pin" /* onClick={() => console.log('if this were its own component, i could have all the opject info!')}*/>
+        {text === 'Chipotle' ? <Icon icon={locationIcon} className="pin-icon"/> :
+        <Icon icon="ic:baseline-my-location" color="#ff0af0" /> }
+        <p className="pin-text">{text}</p>
+
+    </div>
+)
+
+const UserPin = () => (
+    <div className="pin" /* onClick={() => console.log('if this were its own component, i could have all the opject info!')}*/>
+        <Icon icon="ic:baseline-my-location" color="#ff0af0" />
+        <p className="pin-text">{text}</p>
+
+    </div>
+)
+
+
+
+export default function LocationMap({ location, zoomLevel }) {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        console.log('HAAI');
+        dispatch({ type: 'FETCH_LOCATIONS' })
+    }, [])
+
+    const locations = useSelector(store => store.locations)
+
+    console.log(locations);
+
+    const handleClick = () => {
+        console.log('Hewpoooo');
+    }
+
+    return (
+        <>
+            <div className="map">
+                <h2 className="map-h2">Skyway Savor!</h2>
+
+                <div className="google-map">
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: 'AIzaSyAq_lGv4XjzCddzO_oKkBx5j2drPXR8U5A' }}
+                        defaultCenter={
+                            {
+                                address: '301 S 4th Ave #577, Minneapolis, MN 55415',
+                                lat: 44.9780,
+                                lng: -93.2635,
+                            }}
+                        defaultZoom={15}
+                    >
+                        {/* <img src="./Ihavenocluewhatimdoing.png" alt='I have no idea what im doing'></img> */}
+                        {locations.map(marker => (
+                            <LocationPin
+                                lat={marker.lat}
+                                lng={marker.lng}
+                                text={marker.name}
+                                onClick={handleClick}
+                            />
+                        ))}
+                        {/* <LocationPin
+                            lat={44.9780}
+                            lng={-93.2635}
+                            text={'prime digital academy'}
+                        /> */}
+                    </GoogleMapReact>
+                </div>
+            </div>
+        </>
+    )
+}
