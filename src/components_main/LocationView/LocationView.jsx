7213bc4f9dd78eeaba8a2592f8b6410a3e0244cb
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-24 14:49:55 -0500

    this will allow the user to toggle between seeing the the image of a location and the locations map

--- src/components_main/LocationView/LocationView.jsx
+++ src/components_main/LocationView/LocationView.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-
+import { useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 
 import UserReviews from '../UserReviews/UserReviews';
 import LocationReviews from '../LocationReviews/LocationReviews';
+import LocationMap from '../MainMap/LocationMap';
 import ReviewForm from '../ReviewForm/ReviewForm';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
@@ -13,6 +14,8 @@ function LocationView() {
 
   const id = useParams().id
   const dispatch = useDispatch();
+
+  const [map, setMap] = useState(false)
 
   useEffect(() => {
     dispatch({
@@ -25,12 +28,18 @@ function LocationView() {
   })
   }, [])
 
+  const toggleMap =()=>{
+    console.log('boops');
+    setMap(!map)
+  }
+
 
   // const reviews = useSelector(store => store.locationReviews);
   const location_details = useSelector(store => store.details);
 
-  const handleClick = () => {
-    console.log('maur clickiibhss :)');
+  const handleClick = (map) => {
+    console.log('maur clickiibhss :)', map);
+   
   }
 
 
@@ -40,10 +49,15 @@ function LocationView() {
   return (
     <>
       <h1>{details?.name}</h1>
-      <img src={details?.image_url} alt="" width={375} />
+      {map ? <img src={details?.image_url} alt="" width={375} /> :
+        <LocationMap/>
+      }
+      {/* <img src={details?.image_url} alt="" width={375} />
+      <LocationMap/> */}
       <p>{details?.description}</p>
       <h3>{details?.price_status}</h3>
       <button onClick={handleClick}>Add Review</button>
+      <button onClick={() => toggleMap(map)}>Show map</button>
       <ReviewForm details={location_details} />
       <LocationReviews   />
     </>
