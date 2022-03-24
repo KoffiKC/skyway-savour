d9701bdf913a63c9b44936fe44c768e13d7eeda0
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-24 09:53:32 -0500

    basic app nav component created

---
+++ src/components_main/AppNav/AppNav.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+function AppNav () {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default AppNav
