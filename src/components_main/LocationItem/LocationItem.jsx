209b6212cb44bb029ea3cee3d5ef4f3d02aa5af4
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-03-26 20:18:54 -0500

    card material created for locations on main map

--- src/components_main/LocationItem/LocationItem.jsx
+++ src/components_main/LocationItem/LocationItem.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-import {
-    HashRouter as Router,
-    Redirect,
-    Route,
-    Switch,
-} from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Rating from '@mui/material/Rating';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
@@ -37,10 +37,26 @@ function LocationItem({ local }) {
 
     return (
         <>
-            <h1>{local?.name}</h1>
-            <h3>{local?.description}</h3>
-            <button onClick={handleClick}>MORE INFO</button>
-            <button onClick={showOnMap}>show on map</button>
+            <Card sx={{ maxWidth: 345, maxHeight: 320 }}>
+                <CardMedia
+                    component="img"
+                    alt={local?.name}
+                    height="200"
+                    image={local?.image_url}
+                />
+                <CardContent sx={{ p: 0 }} >
+                    <Button 
+                    color="secondary" 
+                    size="large"
+                    onClick={handleClick}>Details</Button>
+                    <Button
+                    size="extra-large"
+                    color="secondary"
+                    onClick={showOnMap}
+                    endIcon={<LocationOnIcon />}>Map</Button>
+                    <Rating name="read-only" value={local.average} sx={{ fontSize: 65 }} readOnly />
+                </CardContent>
+            </Card>
         </>
     )
 }
