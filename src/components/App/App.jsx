0a0a51dc2fe984cea54fe88e20b698cfa60646fe
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-06-04 20:29:01 -0500

    moved and commented out unused but potentially useful code in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,12 +9,9 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
-import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
-import taco from './Untitled46_20220329154629.png'
-
+import logo from './Untitled46_20220329154629.png'
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
@@ -23,74 +20,31 @@ import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
 
 // main_components
-import MainMap from '../../components_main/MainMap/MainMap';
 import ProfileView from '../../components_main/ProfileView/ProfileView';
 import LocationView from '../../components_main/LocationView/LocationView';
 import LocationsList from '../../components_main/LocationsList/LocationsList';
 
-//Drawer components UNUSED
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 
-const drawerWidth = 393;
-
-// UNUSED
-const Left = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(0),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      margin: 0,
-      padding: 0,
-    }),
-  }),
-);
 
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
   const history = useHistory();
-
+  
   const user = useSelector(store => store.user);
   const locations = useSelector(store => store.locations);
   const details = useSelector(store => store.details);
-
+  
   useEffect(() => {
     dispatch({ type: 'FETCH_USER' });
-    // dispatch({ type: 'FETCH_LOCATIONS'});
   }, [dispatch]);
-
-  //Drawer functionality
-  const [openLeft, setOpenLeft] = React.useState(false);
-  const [openRight, setOpenRight] = React.useState(false);
-
-  const handleDrawerRight = () => {
-    setOpenRight(!openRight);
-  };
-
-  const handleDrawerleft = () => {
-    setOpenLeft(!openLeft);
-  };
-
-  const handleDrawerClose = () => {
-    setOpenLeft(false);
-  };
-
-  console.log('values on main app user:', user, 'locations:', locations,);
+  
+  
+  // console.log('values on main app user:', user, 'locations:', locations,);
   return (
     <Router>
-      {user.id ? <img className='mini-logo' onClick={() => history.push('/about')} src={taco} alt='logo' /> : <></>}
+      {user.id ? <img className='mini-logo' onClick={() => history.push('/about')} src={logo} alt='logo' /> : <></>}
 
       <div>
         <Switch>
@@ -102,36 +56,34 @@ function App() {
             // shows AboutPage at all times (logged in or not)
             exact
             path="/about"
-          >
+            >
             <AboutPage />
           </Route>
 
           {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+          Even though it seems like they are different pages, the user is always on localhost:3000/user */}
           <ProtectedRoute
             // logged in shows UserPage else shows LoginPage
             exact
             path="/user"
-          >
+            >
             <UserPage />
-
-
           </ProtectedRoute>
 
           <ProtectedRoute
             // logged in shows InfoPage else shows LoginPage
             exact
             path="/info"
-          >
+            >
             <InfoPage />
           </ProtectedRoute>
 
           <Route
             exact
             path="/login"
-          >
+            >
             {user.id ?
               // If the user is already logged in, 
               // redirect to the /home page
@@ -145,7 +97,7 @@ function App() {
           <Route
             exact
             path="/registration"
-          >
+            >
             {user.id ?
               // If the user is already logged in, 
               // redirect them to the /user page
@@ -159,7 +111,7 @@ function App() {
           <Route
             exact
             path="/home"
-          >
+            >
             {user.id ?
               // depending on if the user is logged in
               // this will render either the main view or the sign up page
@@ -174,7 +126,7 @@ function App() {
             // logged in shows InfoPage else shows LoginPage
             exact
             path="/profile"
-          >
+            >
             <ProfileView />
           </ProtectedRoute>
 
@@ -198,3 +150,50 @@ function App() {
 }
 
 export default App;
+
+// // UNUSED CODE
+// import Nav from '../Nav/Nav';
+
+// //Drawer functionality
+// const [openLeft, setOpenLeft] = React.useState(false);
+// const [openRight, setOpenRight] = React.useState(false);
+
+// const handleDrawerRight = () => {
+//   setOpenRight(!openRight);
+// };
+
+// const handleDrawerleft = () => {
+//   setOpenLeft(!openLeft);
+// };
+
+// const handleDrawerClose = () => {
+//   setOpenLeft(false);
+// };
+
+
+// //Drawer components 
+// import { styled, useTheme } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Drawer from '@mui/material/Drawer';
+
+// const drawerWidth = 393;
+
+// const Left = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+//   ({ theme, open }) => ({
+//     flexGrow: 1,
+//     padding: theme.spacing(0),
+//     transition: theme.transitions.create('margin', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     marginLeft: `-${drawerWidth}px`,
+//     ...(open && {
+//       transition: theme.transitions.create('margin', {
+//         easing: theme.transitions.easing.easeOut,
+//         duration: theme.transitions.duration.enteringScreen,
+//       }),
+//       margin: 0,
+//       padding: 0,
+//     }),
+//   }),
+// );
